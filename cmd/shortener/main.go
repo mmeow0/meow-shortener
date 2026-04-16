ddbc457560e5efc848aab67fd6d44a16873e82bd
Айгуль Тумеркина <ashtumerkina@nsc.media>
2026-04-17 02:33:58 +0300

    Инкремент 23

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/mmeow0/meow-shortener/internal/app"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
+func na(s string) string {
+	if s == "" {
+		return "N/A"
+	}
+	return s
+}
+
 func main() {
+	fmt.Printf("Build version: %s\n", na(buildVersion))
+	fmt.Printf("Build date: %s\n", na(buildDate))
+	fmt.Printf("Build commit: %s\n", na(buildCommit))
+
 	app, err := app.InitializeApp()
 	if err != nil {
 		log.Fatal(err)
